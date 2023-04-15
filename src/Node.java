9e0c22d20b5d696364f8e10ae0f094da1209c786
Harshitsourav <harshit177.sourav@gmail.com>
2023-04-15 18:32:23 +0530

    Initial commit

---
+++ src/Node.java
@@ -0,0 +1,21 @@
+public class Node {
+    private int data;
+    private Node nextNode;
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    public int getData(){
+        return this.data = data;
+    }
+    public Node getNode(){
+        return this.nextNode;
+    }
+    public void setData(int data){
+        this.data = data;
+    }
+    public void setNextNode(Node nextNode){
+        this.nextNode = nextNode;
+    }
+}
