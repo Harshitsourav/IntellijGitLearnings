fb1a5d1dadcaf3c0204dc9a183762bcea3008ce7
Harshitsourav <harshit177.sourav@gmail.com>
2023-04-15 18:45:25 +0530

    Changed the input value from 40 to 50

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Node head = new Node(30);
+        head.setNextNode(new Node(50));
+        System.out.println((head.getNode()).getData());
     }
 }
